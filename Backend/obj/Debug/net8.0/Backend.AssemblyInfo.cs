c66c33b16e20c038a3875fab34f62640ae17d9c4
Muhammad Salman Amir <mmsa155j3@example.com>
2024-11-10 21:52:17 +0500

    check

--- Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f7b1a73f1f3777d18f9b39cf193714df0e88b21")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15cebbd57d0eaf6e122ca3c17363d50cc0248100")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
